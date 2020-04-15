f03e021e2e1ed79aa8ec41a8aa655a83d6ae6420
EtiennePASSOT <etienne.passot@epitech.eu>
2020-04-15 10:46:41 +0200

    Add MathUtils
    adding MathUtils to calculate min, max, average and standard deviation of an array

---
+++ project/app/MathUtils.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App;
+
+final class MathUtils
+{
+    static function average($array)
+    {
+        return array_sum($array) / count($array);
+    }
+
+    static function standardDeviation($array)
+    {
+        $average = self::average($array);
+        foreach ($array as $i) { 
+            $variance += pow(($i - $average), 2);
+        }
+        return (float) sqrt($variance / count($array));
+    }
+
+    static function max($array) {
+        return max($array);
+    }
+
+    static function min($array) {
+        return min($array);
+    }
+}
