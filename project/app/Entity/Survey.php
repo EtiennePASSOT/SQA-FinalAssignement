95be2c7c6b7e1c016299e7e6cb9b4693ca642ff9
EtiennePASSOT <etienne.passot@epitech.eu>
2020-04-15 11:18:31 +0200

    add statistics for survey
    adding average, min, max, stdev for survey

--- project/app/Entity/Survey.php
+++ project/app/Entity/Survey.php
@@ -2,6 +2,7 @@
 
 namespace App\Entity;
 
+use App\MathUtils;
 use App\Entity\Question;
 
 final class Survey
@@ -43,4 +44,38 @@ final class Survey
         return false;
     }
 
+    function getAllMarks() {
+        $marks = [];
+        foreach ($this->getQuestions() as $key => $question) {
+            $marks = array_merge($marks, $question->getAnswers()->getMarks());
+        }
+        return $marks;
+    }
+
+    function average()
+    {
+        return MathUtils::average($this->getAllMarks());
+    }
+
+    function standardDeviation()
+    {
+        return MathUtils::standardDeviation($this->getAllMarks());
+    }
+
+    function max() {
+        return MathUtils::max($this->getAllMarks());
+    }
+
+    function min() {
+        return MathUtils::min($this->getAllMarks());
+    }
+
+    function statistics() {
+        return array(
+            "average" => $this->average(),
+            "standardDeviation" => $this->standardDeviation(),
+            "max" => $this->max(),
+            "min" => $this->min()
+        );
+    }
 }
